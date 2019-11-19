8156c6535114c952fb8153bb8994bc7ea8bb9378
mbhandari26 <56984301+mbhandari26@users.noreply.github.com>
2019-11-19 11:30:11 -0600

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,18 +1,5 @@
-//****************************************************************************************************
-//
-//		File:					Customer.h
-//		
-//		Student:				Manish Bhandari
-//
-//		Assignment:	 			Program  # 11
-//
-//		Course Name:			Data Structures   II 
-//
-//		Course Number:			COSC 3100 - 01
-//
-//		Due:					Nov 17, 2019
-//
-//
+
+
 //		This program uses a heap class to establish a priority queue of 
 //		overbooked customers for an airline company.
 //		
